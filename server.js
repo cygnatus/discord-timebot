8734e41dc2924ed13c74b8c4b3c588ab393c811f
Glitch (discord-bot-example) <none>
2017-11-26 15:13:08 +0000

    👗📕 Use appropriate function calls instead

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const moment = require("moment-timezone");
 const clock = require("node-emoji-clock");
 
-const bot = new Eris(process.env.DISCORD_BOT_TOKEN, {}, {
+const bot = new Eris.CommandClient(process.env.DISCORD_BOT_TOKEN, {}, {
     "description": "Show the current time in three timezones.",
     "owner": "Cygnatus",
     "prefix": "!"
@@ -15,18 +15,31 @@ bot.on("ready", () => {
         moment().tz("America/Los_Angeles").format("HH:mm z"));
 });
 
-// When a message is created
-bot.on("messageCreate", (msg) => { 
-    if(msg.content.includes("!timebot")) { // that reads "!timebot"
+bot.registerCommand("timebot", (msg, args) => {
+    if (args.length === 0) {
         var ct = moment().tz("Europe/Copenhagen");
         var et = moment().tz("America/New_York");
         var pt = moment().tz("America/Los_Angeles");
 
-        bot.createMessage(msg.channel.id,
-            clock.timeToEmoji(ct) + " \`" + ct.format("HH:mm z") + "\`\n" +
+        var times = clock.timeToEmoji(ct) + " \`" + ct.format("HH:mm z") + "\`\n" +
             clock.timeToEmoji(et) + " \`" + et.format("HH:mm z") + "\`\n" +
-            clock.timeToEmoji(pt) + " \`" + pt.format("HH:mm z") + "\`" );
+            clock.timeToEmoji(pt) + " \`" + pt.format("HH:mm z") + "\`";
+
+        return times
+    } else {
+        return "I only respond to \`!timebot\` for now.";
     }
+}, {
+    "argsRequired": false,
+    "caseInsensitive": false,
+    "cooldown": 5000,
+    "cooldownMessage": "Please wait 5 seconds between invoking \`!timebot\`.",
+    "deleteCommand": false,
+    "description": "Show time in CET, ET, and PT.",
+    "dmOnly": false,
+    "fullDescription": "Timebot shows the current time in three Western timezones: CET, ET, and PT.",
+    "usage": "!timebot"
 });
+bot.registerCommandAlias("timezones", "timebot");
 
 bot.connect(); // Connect to server (Discord)
